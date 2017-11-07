89ec827d09a17d8c3832febf31675b865f640db1
iamqingmei <iamqingmei@gmail.com>
2017-11-07 15:31:33 +0800

    same as previous commit

--- wear/src/main/java/ageha/gesturecollector/DeviceClient.java
+++ wear/src/main/java/ageha/gesturecollector/DeviceClient.java
@@ -1,22 +1,18 @@
 package ageha.gesturecollector;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.SparseLongArray;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -27,9 +23,7 @@ class DeviceClient {
     private static final String TAG = "DeviceClient";
     private static final int CLIENT_CONNECTION_TIMEOUT = 15000;
     private SparseLongArray lastSensorData;
-    private int send_count = 0;
     private static DeviceClient instance;
-    private static ArrayList<Integer> usefulSensor = new ArrayList<>(Arrays.asList(1, 2, 4, 11, 3, 26, 17, 9, 10));
     public static DeviceClient getInstance(Context context) {
         if (instance == null) {
             instance = new DeviceClient(context.getApplicationContext());
@@ -50,11 +44,14 @@ class DeviceClient {
         lastSensorData = new SparseLongArray();
     }
 
-//    public void setSensorFilter(int filterId) {
-//        Log.d(TAG, "Now filtering by sensor: " + filterId);
-//
-//        this.filterId = filterId;
-//    }
+    void sendTag(final String tagName){
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                sendTagInBackground(tagName);
+            }
+        });
+    }
 
     void sendSensorData(final String sensorName, final int sensorType, final int accuracy, final long timestamp, final float[] values) {
         long t = System.currentTimeMillis();
@@ -76,42 +73,13 @@ class DeviceClient {
         });
     }
 
-    void sendSensorDataPack(final String sensorDataPack){
-        Log.i(TAG, "sendSensorDataPack");
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                sendPackedSensorDataInBackground(sensorDataPack);
-            }
-        });
-    }
-    void sendCount(){
-        Log.i(TAG, "sendCount: " + send_count);
-        executorService.submit(new Runnable() {
-            @Override
-            public void run() {
-                PutDataMapRequest dataMap = PutDataMapRequest.create("/sensors/sensorcount");
-                Log.i(TAG, "run() " + send_count);
-                dataMap.getDataMap().putInt(DataMapKeys.SENSORCOUNT, send_count);
-                PutDataRequest putDataRequest = dataMap.asPutDataRequest();
-                send(putDataRequest);
-            }
-        });
-        send_count += 1;
-    }
-    private void sendPackedSensorDataInBackground(String sensorDataPack){
-        PutDataMapRequest dataMap = PutDataMapRequest.create("/sensors/sensorpacks");
-        dataMap.getDataMap().putString(DataMapKeys.SENSORPACK, sensorDataPack);
-        PutDataRequest putDataRequest = dataMap.asPutDataRequest();
+    private void sendTagInBackground(String tagName){
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/sensors/tag");
+        putDataMapReq.getDataMap().putString(DataMapKeys.TAG, tagName);
+        PutDataRequest putDataRequest = putDataMapReq.asPutDataRequest();
         send(putDataRequest);
     }
-
     private void sendSensorDataInBackground(String sensorName, int sensorType, int accuracy, long timestamp, float[] values) {
-//        if (sensorType == filterId) {
-//            Log.i(TAG, "Sensor " + sensorType + " = " + Arrays.toString(values));
-//        } else {
-//            Log.d(TAG, "Sensor " + sensorType + " = " + Arrays.toString(values));
-//        }
 
         PutDataMapRequest dataMap = PutDataMapRequest.create("/sensors/datapoint/" + sensorType);
         dataMap.getDataMap().putString(DataMapKeys.SENSORNAME, sensorName);
